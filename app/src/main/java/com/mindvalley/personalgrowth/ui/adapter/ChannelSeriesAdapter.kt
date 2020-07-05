24fa6739a62a7b44f80f6fbfcaa7462214f69bb9
taiwoadebayo <mikelis135@gmail.com>
2020-07-05 20:06:41 +0100

    adjusted the package of adapters

---
+++ app/src/main/java/com/mindvalley/personalgrowth/ui/adapter/ChannelSeriesAdapter.kt
@@ -0,0 +1,57 @@
+package com.mindvalley.personalgrowth.ui.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.mindvalley.personalgrowth.R
+import com.mindvalley.personalgrowth.local.entity.Series
+import kotlinx.android.synthetic.main.channel_series_item.view.*
+
+class ChannelSeriesAdapter(
+    private val context: Context,
+    private val channelSeriesList: List<Series>
+) :
+    RecyclerView.Adapter<ChannelSeriesAdapter.SeriesViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): SeriesViewHolder {
+
+        return SeriesViewHolder(
+            LayoutInflater.from(context).inflate(
+                R.layout.channel_series_item,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return channelSeriesList.size
+    }
+
+    override fun onBindViewHolder(holder: SeriesViewHolder, position: Int) {
+        holder.bind(channelSeriesList[position], context)
+    }
+
+    class SeriesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val channelSeriesTitle: TextView = itemView.channelSeriesMediaTitleTxt
+        private val channelSeriesImage: ImageView = itemView.channelSeriesMediaPhotoImg
+
+        private val requestOptions = RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL)
+        fun bind(series: Series, context: Context) {
+            channelSeriesTitle.text = series.title
+            Glide.with(context).load(series.coverAsset?.url).apply(requestOptions)
+                .into(channelSeriesImage)
+
+        }
+    }
+}
---
+++ app/src/main/java/com/mindvalley/personalgrowth/ui/adapter/NewEpisodesAdapter.kt
@@ -0,0 +1,57 @@
+package com.mindvalley.personalgrowth.ui.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.mindvalley.personalgrowth.R
+import com.mindvalley.personalgrowth.local.entity.Course
+import kotlinx.android.synthetic.main.new_episodes_item.view.*
+
+class NewEpisodesAdapter(
+    private val context: Context,
+    private val episodeList: List<Course>
+) :
+    RecyclerView.Adapter<NewEpisodesAdapter.EpisodeViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): EpisodeViewHolder {
+
+        return EpisodeViewHolder(
+            LayoutInflater.from(context).inflate(
+                R.layout.new_episodes_item,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return episodeList.size
+    }
+
+    override fun onBindViewHolder(holder: EpisodeViewHolder, position: Int) {
+        holder.bind(episodeList[position], context)
+    }
+
+    class EpisodeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val channelTitle: TextView = itemView.channelTitleTxt
+        private val courseTitle: TextView = itemView.courseTitleTxt
+        private val episodeImage: ImageView = itemView.episodePhotoImg
+
+        fun bind(course: Course, context: Context) {
+            channelTitle.text = course.channel?.title
+            courseTitle.text = course.title
+            Glide.with(context).load(course.coverAsset?.url).apply(RequestOptions().override(500, 500))
+                .into(episodeImage)
+
+        }
+    }
+}
